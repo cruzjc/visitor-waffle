0579c70f4b3a1d5b5e47238c59a7cfeff9e24bf9
cruzjc <jeanclydecruz@gmail.com>
2025-03-23 21:21:34 -1000

    Add obfuscated Firebase config

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+// Obfuscated Firebase configuration
+const _0x5f3d=['YXBw','Y3Vyc29yLXZpc2l0b3Itd2FmZmxl','QUl6YVN5RDJxSjlOd1FXSkxFWVFaemVkeVhfV1F3WWkxTVNlQ0RR','Y3Vyc29yLXZpc2l0b3Itd2FmZmxlLmZpcmViYXNlYXBwLmNvbQ==','aHR0cHM6Ly9jdXJzb3ItdmlzaXRvci13YWZmbGUtZGVmYXVsdC1ydGRiLmZpcmViYXNlaW8uY29tLw==','Y3Vyc29yLXZpc2l0b3Itd2FmZmxlLmZpcmViYXNlc3RvcmFnZS5hcHA=','MjQwODc1NDU1Mjgz','MToyNDA4NzU0NTUyODM6d2ViOjE2YjRjZGNiNDg4OTZhM2M0YjM0NDY=','Ry1CRDU1Q1RKMFA4'];
+
+// Decode configuration
+const _0x2f = function(s) { return atob(s); };
+const firebaseConfig = {
+    apiKey: _0x2f(_0x5f3d[2]),
+    authDomain: _0x2f(_0x5f3d[3]),
+    databaseURL: _0x2f(_0x5f3d[4]),
+    projectId: _0x2f(_0x5f3d[1]),
+    storageBucket: _0x2f(_0x5f3d[5]),
+    messagingSenderId: _0x5f3d[6],
+    appId: _0x2f(_0x5f3d[7]),
+    measurementId: _0x5f3d[8]
+}; 
